e0bdc8a5207e57497a786029e4266b91ac62050b
jordan <jordan.jakisa@gmail.com>
2024-03-11 13:25:51 +0300

    🎨 Color and theme issues

--- app/src/main/java/com/keru/pdfcreator/ui/theme/Type.kt
+++ app/src/main/java/com/keru/pdfcreator/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.keru.pdfcreator.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight

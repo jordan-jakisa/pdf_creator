68edab06292e2c2c8c8e48b26768167b43ac81e4
jordan <jordan.jakisa@gmail.com>
2024-03-11 13:42:34 +0300

    🎨 Added file provider configurations

--- app/src/main/java/com/keru/pdfcreator/utils/ExtensionFunctions.kt
+++ app/src/main/java/com/keru/pdfcreator/utils/ExtensionFunctions.kt
@@ -5,16 +5,43 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
+import androidx.core.content.FileProvider
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.TimeUnit
 
 fun Context.openPdfFile(pdfUri: Uri) {
-    val intent = Intent(Intent.ACTION_VIEW)
-    intent.setDataAndType(pdfUri, "application/pdf")
-    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+    val path = pdfUri.path
+    path?.let {
+        val externalUri = FileProvider.getUriForFile(this, "$packageName.provider", File(path))
+        val shareIntent =
+            Intent(Intent.ACTION_SEND).apply {
+                putExtra(Intent.EXTRA_STREAM, externalUri)
+                type = "application/pdf"
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            }
+        startActivity(Intent.createChooser(shareIntent, "Open pdf"))
+    }
+}
 
-    try {
-        startActivity(intent)
-    } catch (e: ActivityNotFoundException) {
-        // Handle the case where a PDF viewer app is not installed on the device
-        Toast.makeText(this, "No PDF viewer found", Toast.LENGTH_SHORT).show()
+fun Long.getTimeAgo(): String {
+    val currentTimeMillis = System.currentTimeMillis()
+    val timeDifferenceMillis = currentTimeMillis - this
+
+    val seconds = TimeUnit.MILLISECONDS.toSeconds(timeDifferenceMillis)
+    val minutes = TimeUnit.MILLISECONDS.toMinutes(timeDifferenceMillis)
+    val hours = TimeUnit.MILLISECONDS.toHours(timeDifferenceMillis)
+    val days = TimeUnit.MILLISECONDS.toDays(timeDifferenceMillis)
+
+    return when {
+        seconds < 60 -> "$seconds seconds ago"
+        minutes < 60 -> "$minutes minutes ago"
+        hours < 24 -> "$hours hours ago"
+        days == 1L -> "yesterday"
+        days < 7 -> "$days days ago"
+        else -> {
+            SimpleDateFormat("MMM d, yyyy", Locale.getDefault()).format(this)
+        }
     }
 }
